0a54371da71b04052ef4a55ef16540264389f6e5
SLB SMKMIL <129854483+slbsmkmil@users.noreply.github.com>
2023-04-06 20:38:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -23,4 +23,18 @@ modeSwitch.addEventListener("click" , () =>{
         modeText.innerText = "Dark mode";
         
     }
-});
+})
+
+var swiper = new Swiper(".mySwiper", {
+    slidePreView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+  });
